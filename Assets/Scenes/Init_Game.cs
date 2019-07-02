3983e894c544cfd6fd85388a6db293d9889b439a
raccoon2709 <samuel.kloss@web.de>
2019-07-02 21:00:02 +0200

    First Version

---
+++ Assets/Scenes/Init_Game.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Init_Game : MonoBehaviour
+{
+    public Button button;
+    
+    void Awake()
+    {
+       button.onClick.AddListener(TaskOnClick);
+    }
+    void TaskOnClick()
+    {
+        SceneManager.LoadScene("Startgame");
+    }
+
+}
